0224dee432c530502ee2f8d1ea8437ed8a155812
Tom Cant <tomcant@gmail.com>
2019-06-27 13:01:31 +0100

    Added a script for triggering rebuilds.

---
+++ rebuild.sh
@@ -0,0 +1,11 @@
+curl -X PUT -H "Authorization: token 74212bd9ba78f6ebe67cc51ef6585a49ab4c0ecd" \
+  https://api.github.com/repos/tomcant/tomcant.github.io/contents/.publish -d \
+    '{
+        "message": "Rebuild GitHub Pages",
+        "committer": {
+            "name": "RebuildBot",
+            "email": "rebuildbot@tomcant.dev"
+        },
+        "content": "LnB1Ymxpc2gK",
+        "sha": "3f949857e8ed4cb106f9744e40b638a7aabf647f"
+    }'
